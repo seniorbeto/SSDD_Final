9c847d952fb630ea4b1b9416b20a313d2ead5260
seniorbeto <100471939@alumnos.uc3m.es>
2025-05-01 17:50:56 +0200

    estructura de ficheros de test

--- src/test.sh
+++ src/test.sh
@@ -10,27 +10,47 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-mkdir -p test_files
-rm -f test_files/*.log
+mkdir -p test_files/output/logs
+rm -f test_files/output/logs/*.log
+
+run_test() {
+  local test_name=$1
+  local input_file=$2
+  local expected_file=$3
+  local output_file="test_files/output/${test_name}.output"
+
+  # Ejecutar el cliente, redirigiendo entrada desde $input_file y guardando salida
+  python3 client/client.py -s $SERVER_IP -p $SERVER_PORT < "$input_file" > "$output_file" 2>/dev/null
+
+  # Comparar resultado con lo esperado
+  if diff -b -B -q <(sed -E 's/[0-9]{4,5}/PORT/g' "$output_file") \
+                <(sed -E 's/[0-9]{4,5}/PORT/g' "$expected_file") >/dev/null; then
+      echo -e "${GREEN}Test '$test_name' passed.${NC}"
+    else
+      echo -e "${RED}Test '$test_name' failed.${NC}"
+      echo -e "${YELLOW}Differences:${NC}"
+      diff -b -B <(sed -E 's/[0-9]{4,5}/PORT/g' "$output_file") \
+           <(sed -E 's/[0-9]{4,5}/PORT/g' "$expected_file")
+    fi
+}
+
 
 # LOGGER (servicio rpc)
-./logger/cmake-build-release/logger > test_files/logger.log 2>&1 &
+./logger/cmake-build-release/logger > test_files/output/logs/logger.log 2>&1 &
 LOGGER_PID=$!
 
 # SERVER
 export LOG_RPC_IP=$RPC_SERVICE_IP
-./server/cmake-build-release/server $SERVER_PORT > test_files/server.log 2>&1 &
+./server/cmake-build-release/server $SERVER_PORT > test_files/output/logs/server.log 2>&1 &
 SERVER_PID=$!
 
 # WEB SERVICE
-python3 web_server/web_server.py > test_files/web_service.log 2>&1 &
+python3 web_server/web_server.py > test_files/output/logs/web_service.log 2>&1 &
 WEB_SERVICE_PID=$!
 
-# Definir el trap antes del bucle
-trap 'echo -e "\n\n${RED}Stopping services...${NC}"; kill -TERM $LOGGER_PID 2>/dev/null; kill -TERM $SERVER_PID 2>/dev/null; kill -TERM $WEB_SERVICE_PID 2>/dev/null; exit' INT
+# tests
+run_test "test_1" "test_files/input/test_1.txt" "test_files/expected/test_1_expected.txt"
 
-# Esperar a Ctrl+C
-echo -e "\n\n${YELLOW}Press Ctrl+C to stop the services...${NC}"
-while true; do
-  sleep 1
-done
+kill $SERVER_PID 2>/dev/null
+kill $LOGGER_PID 2>/dev/null
+kill $WEB_SERVICE_PID 2>/dev/null
